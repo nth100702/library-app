d0156c754b38a9e4e0022c891aaf788f836f43db
Trung Hieu Nguyen <nth100702@gmail.com>
2024-02-18 23:33:58 +0700

    Search page

--- 03-frontend/react-library/src/layouts/HomePage/components/Carousel.tsx
+++ 03-frontend/react-library/src/layouts/HomePage/components/Carousel.tsx
@@ -1,6 +1,7 @@
 import { ReturnBook } from "./ReturnBook"
 import { useEffect,useState } from "react"
 import BookModel from "../../../models/BookModel"
+import { SpinnerLoading } from "../../Utils/SpinnerLoading";
 
 export const Carousel =()=>{
 
@@ -49,9 +50,7 @@ export const Carousel =()=>{
 
     if(isLoading){
         return(
-            <div className="container m-5">
-                <p>Loading...</p>
-            </div>
+            <SpinnerLoading/>
         )
     }
 
