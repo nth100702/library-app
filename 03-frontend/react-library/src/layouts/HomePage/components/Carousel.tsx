a065f9b084ff075c9b360a0d07afa4ca058c7c4c
Trung Hieu Nguyen <nth100702@gmail.com>
2024-09-21 17:21:25 +0700

    implement Review Page

--- 03-frontend/react-library/src/layouts/HomePage/components/Carousel.tsx
+++ 03-frontend/react-library/src/layouts/HomePage/components/Carousel.tsx
@@ -2,6 +2,7 @@ import { ReturnBook } from "./ReturnBook"
 import { useEffect,useState } from "react"
 import BookModel from "../../../models/BookModel"
 import { SpinnerLoading } from "../../Utils/SpinnerLoading";
+import { Link } from "react-router-dom";
 
 export const Carousel =()=>{
 
@@ -113,7 +114,7 @@ export const Carousel =()=>{
                 </div>
             </div>
             <div className="homepage-carousel-title mt-3">
-                <a className="btn btn-outline-secondary btn-lg" href="#">View more</a>
+                <Link className="btn btn-outline-secondary btn-lg" to="/search">View more</Link>
             </div>
         </div>
     )
