ab380b22209143b302dd152112e6c9db214da137
Trung Hieu Nguyen <nth100702@gmail.com>
2024-10-12 21:42:05 +0700

    HTTPS React

--- 03-frontend/react-library/src/lib/oktaConfig.ts
+++ 03-frontend/react-library/src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
     clientId: process.env.REACT_APP_CLIENT_ID,
     issuer: process.env.REACT_APP_ISSUER,
-    redirectUri: 'http://localhost:3000/login/callback',
+    redirectUri: 'https://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true,
