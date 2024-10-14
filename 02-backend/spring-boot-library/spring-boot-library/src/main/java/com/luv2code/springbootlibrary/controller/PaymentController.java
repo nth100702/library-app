11c5e2c98bb7036459c5cd470b02e0314e44c859
Trung Hieu Nguyen <nth100702@gmail.com>
2024-10-14 22:55:27 +0700

    Payment Controller Endpoints

--- 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/controller/PaymentController.java
+++ 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/controller/PaymentController.java
@@ -1,10 +1,13 @@
 package com.luv2code.springbootlibrary.controller;
 
+import com.luv2code.springbootlibrary.requestmodels.PaymentInfoRequest;
 import com.luv2code.springbootlibrary.service.PaymentService;
+import com.luv2code.springbootlibrary.utils.ExtractJWT;
+import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("https://localhost:3000")
 @RestController
@@ -16,5 +19,24 @@ public class PaymentController {
     @Autowired
     public PaymentController(PaymentService paymentService){
         this.paymentService = paymentService;
+    }
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfoRequest paymentInfoRequest) throws Exception {
+
+        PaymentIntent paymentIntent = paymentService.createPaymentIntent(paymentInfoRequest);
+        String paymentStr = paymentIntent.toJson();
+
+        return new ResponseEntity<>(paymentStr, HttpStatus.OK);
+    }
+
+    @PutMapping("/payment-complete")
+    public ResponseEntity<String> stripePaymentComplete(@RequestHeader(value = "Authorization") String token) throws Exception {
+
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        if(userEmail==null) {
+            throw new Exception("User email is missing");
+        }
+        return paymentService.stripePayment(userEmail);
     }
 }
