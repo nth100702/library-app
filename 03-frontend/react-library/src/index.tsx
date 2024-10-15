8b84b61f400a66c45d5d7277552d1aaa467e57ee
Trung Hieu Nguyen <nth100702@gmail.com>
2024-10-15 23:23:08 +0700

    PaymentInfoRequest Model

--- 03-frontend/react-library/src/index.tsx
+++ 03-frontend/react-library/src/index.tsx
@@ -6,14 +6,16 @@ import { BrowserRouter } from 'react-router-dom';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js'
 
-const stripePromise = loadStripe('pk_test_51Q9UJj08zh50kAarEQimEv64XQei6leVD6n0e2M5cQaIeiJL0FyAGsKpF6YKowiubyHz7fC8qOqo8HzR32hOFIwZ00YwZEDSyQ')
+const stripePromise = loadStripe('pk_test_51Q9UJj08zh50kAarEQimEv64XQei6leVD6n0e2M5cQaIeiJL0FyAGsKpF6YKowiubyHz7fC8qOqo8HzR32hOFIwZ00YwZEDSyQ');
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
+  <Elements stripe={stripePromise}>
     <App />
+  </Elements>
   </BrowserRouter>
 );
 
