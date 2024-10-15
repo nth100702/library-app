ffebad9e778471d385c5d4419ee2b304357458b7
Trung Hieu Nguyen <nth100702@gmail.com>
2024-10-15 23:19:13 +0700

    Install Stripe dependencies & Configure Index files

--- 03-frontend/react-library/src/index.tsx
+++ 03-frontend/react-library/src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import {App} from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js'
+
+const stripePromise = loadStripe('pk_test_51Q9UJj08zh50kAarEQimEv64XQei6leVD6n0e2M5cQaIeiJL0FyAGsKpF6YKowiubyHz7fC8qOqo8HzR32hOFIwZ00YwZEDSyQ')
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
